8416880c582da6097eeefceabdda0e19dae70e51
Restyled.io <commits@restyled.io>
2022-09-30 15:38:42 +0000

    Restyled by black

--- Programs/binayak-countdown.py
+++ Programs/binayak-countdown.py
@@ -4,8 +4,8 @@ import time
 def countdowntimer(time_sec):
     while time_sec:
         mins, secs = divmod(time_sec, 60)
-        timeform = '{:02d}:{:02d}'.format(mins, secs)
-        print(timeform, end='\r')
+        timeform = "{:02d}:{:02d}".format(mins, secs)
+        print(timeform, end="\r")
         time.sleep(1)
         time_sec -= 1
 
