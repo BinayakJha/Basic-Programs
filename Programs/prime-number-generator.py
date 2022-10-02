98792eca5fcc573672e7487717cfe81747daf9ae
Luca Di Bello <info@lucadibello.ch>
2022-10-02 19:41:57 +0200

    Restyled using autopep8

--- Programs/prime-number-generator.py
+++ Programs/prime-number-generator.py
@@ -1,26 +1,23 @@
-from math import sqrt
-
-
 def isPrime(val: int) -> bool:
-  for n in range(2, val):
-    if val % n == 0:
-      return False
-  return True
+    for n in range(2, val):
+        if val % n == 0:
+            return False
+    return True
 
 
 def primeSequence(N=1000):
-  if N < 2:
-    # Empty list since the fist prime number is 2
-    return []
-  elif N >= 2:
-    primes = []
+    if N < 2:
+        # Empty list since the fist prime number is 2
+        return []
+    elif N >= 2:
+        primes = []
 
-    # Calculate primes using nested for loops
-    for val in range(2, N + 1):
-      # for each value, check if it's a valid prime
-      if isPrime(val):
-        primes.append(val)
-    return primes
+        # Calculate primes using nested for loops
+        for val in range(2, N + 1):
+            # for each value, check if it's a valid prime
+            if isPrime(val):
+                primes.append(val)
+        return primes
 
 
 # Print prime numbers
