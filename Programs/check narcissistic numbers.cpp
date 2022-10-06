909cc3b8dfaac5ef4ac3be445cf23d11682ca1e9
Restyled.io <commits@restyled.io>
2022-10-06 20:04:42 +0000

    Restyled by clang-format

--- Programs/check narcissistic numbers.cpp
+++ Programs/check narcissistic numbers.cpp
@@ -4,40 +4,36 @@
 using namespace std;
 
 // function to count digits
-int countDigit(int n)
-{
-    if (n == 0)
-        return 0;
+int countDigit(int n) {
+  if (n == 0)
+    return 0;
 
-    return 1 + countDigit(n / 10);
+  return 1 + countDigit(n / 10);
 }
 
 // Returns true if n is Narcissistic number
-bool check(int n)
-{
-    // count the number of digits
-    int l = countDigit(n);
-    int dup = n;
-    int sum = 0;
+bool check(int n) {
+  // count the number of digits
+  int l = countDigit(n);
+  int dup = n;
+  int sum = 0;
 
-    // calculates the sum of digits
-    // raised to power
-    while (dup)
-    {
-        sum += pow(dup % 10, l);
-        dup /= 10;
-    }
+  // calculates the sum of digits
+  // raised to power
+  while (dup) {
+    sum += pow(dup % 10, l);
+    dup /= 10;
+  }
 
-    return (n == sum);
+  return (n == sum);
 }
 
 // Driver code
-int main()
-{
-    int n = 1634;
-    if (check(n))
-        cout << "yes";
-    else
-        cout << "no";
-    return 0;
+int main() {
+  int n = 1634;
+  if (check(n))
+    cout << "yes";
+  else
+    cout << "no";
+  return 0;
 }
